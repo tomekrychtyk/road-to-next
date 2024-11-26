7c4bf06309e5f263b989a9d11500c74e5117cb25
Tomek Rychtyk <tomasz.rychtyk@delve.com>
2024-11-26 19:42:53 +0100

    WIP

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,22 +1,9 @@
-"use client";
-
-import { useEffect, useState } from "react";
 import { Heading } from "@/components/heading";
 import { TicketItem } from "@/features/ticket/components/ticket-item";
 import { getTickets } from "@/features/ticket/queries/get-tickets";
-import { Ticket } from "@/features/ticket/types";
 
-const TicketsPage = () => {
-  const [tickets, setTickets] = useState<Ticket[]>([]);
-
-  useEffect(() => {
-    async function fetchTickets() {
-      const result = await getTickets();
-      setTickets(result);
-    }
-
-    fetchTickets();
-  }, []);
+const TicketsPage = async () => {
+  const tickets = await getTickets();
 
   return (
     <div className="flex-1 flex flex-col gap-y-8">
